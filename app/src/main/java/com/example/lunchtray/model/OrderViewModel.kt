db57cf3a240b31dc55f6df8bc2e69a1245082bbf
Ignacio Gonzalez <mylaneza@gmail.com>
2022-09-30 11:26:08 -0500

    Navigation and tests

--- app/src/main/java/com/example/lunchtray/model/OrderViewModel.kt
+++ app/src/main/java/com/example/lunchtray/model/OrderViewModel.kt
@@ -75,7 +75,8 @@ class OrderViewModel : ViewModel() {
             previousEntreePrice = _entree.value?.price!!
         // TODO: if _subtotal.value is not null subtract the previous entree price from the current
         //  subtotal value. This ensures that we only charge for the currently selected entree.
-        _subtotal.value?.minus(previousEntreePrice)
+        if(_subtotal.value != null )
+            _subtotal.value = _subtotal.value!! - previousEntreePrice
         // TODO: set the current entree value to the menu item corresponding to the passed in string
         // TODO: update the subtotal to reflect the price of the selected entree.
         _entree.value = menuItems.get(entree)
@@ -92,7 +93,8 @@ class OrderViewModel : ViewModel() {
         // TODO: if _subtotal.value is not null subtract the previous side price from the current
         //  subtotal value. This ensures that we only charge for the currently selected side.
         //if( _subtotal.value != null)
-        _subtotal.value?.minus( previousSidePrice)
+        if(_subtotal.value != null )
+            _subtotal.value = _subtotal.value!! - previousSidePrice
         // TODO: set the current side value to the menu item corresponding to the passed in string
         // TODO: update the subtotal to reflect the price of the selected side.
         _side.value = menuItems.get(side)
@@ -105,13 +107,13 @@ class OrderViewModel : ViewModel() {
     fun setAccompaniment(accompaniment: String) {
         // TODO: if _accompaniment.value is not null, set the previous accompaniment price to the
         //  current accompaniment price.
-        _accompaniment.value?.apply {
-            previousAccompanimentPrice = price
-        }
+        if( _accompaniment.value != null)
+            previousAccompanimentPrice = _accompaniment.value?.price!!
         // TODO: if _accompaniment.value is not null subtract the previous accompaniment price from
         //  the current subtotal value. This ensures that we only charge for the currently selected
         //  accompaniment.
-        _subtotal.value?.minus(previousAccompanimentPrice)
+        if(_subtotal.value != null )
+            _subtotal.value = _subtotal.value!! - previousAccompanimentPrice
         // TODO: set the current accompaniment value to the menu item corresponding to the passed in
         //  string
         // TODO: update the subtotal to reflect the price of the selected accompaniment.
@@ -128,7 +130,7 @@ class OrderViewModel : ViewModel() {
         //  added item.
         //  Otherwise, set _subtotal.value to equal the price of the item.
         if(_subtotal.value != null)
-            _subtotal.value?.plus(itemPrice)
+            _subtotal.value = _subtotal.value!! + itemPrice
         else
             _subtotal.value = itemPrice
         // TODO: calculate the tax and resulting total
@@ -156,5 +158,8 @@ class OrderViewModel : ViewModel() {
         _subtotal.value = 0.0
         _tax.value = 0.0
         _total.value = 0.0
+        previousEntreePrice = 0.0
+        previousSidePrice = 0.0
+        previousSidePrice = 0.0
     }
 }
--- app/src/main/java/com/example/lunchtray/ui/order/EntreeMenuFragment.kt
+++ app/src/main/java/com/example/lunchtray/ui/order/EntreeMenuFragment.kt
@@ -21,6 +21,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.lunchtray.R
 import com.example.lunchtray.databinding.FragmentEntreeMenuBinding
 import com.example.lunchtray.model.OrderViewModel
 
@@ -68,6 +70,7 @@ class EntreeMenuFragment : Fragment() {
      */
     fun goToNextScreen() {
         // TODO: Navigate to the SideMenuFragment
+        findNavController().navigate(R.id.action_entreeMenuFragment_to_sideMenuFragment)
     }
 
     /**
@@ -75,7 +78,9 @@ class EntreeMenuFragment : Fragment() {
      */
     fun cancelOrder() {
         // TODO: Reset order in view model
+        sharedViewModel.resetOrder()
         // TODO: Navigate back to the [StartFragment] to start over
+        findNavController().navigate(R.id.action_entreeMenuFragment_to_startOrderFragment)
     }
 
     /**
